fdb7e0febf2548fb86e90c0f20d66195d9db66b8
FireLord <firelord.xda@gmail.com>
2024-03-15 14:30:47 +0530

    Initial Commit

---
+++ IosFirebaseLogin/IosFirebaseLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosFirebaseLoginApp.swift
+//  IosFirebaseLogin
+//
+//  Created by admin on 15/03/24.
+//
+
+import SwiftUI
+
+@main
+struct IosFirebaseLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
