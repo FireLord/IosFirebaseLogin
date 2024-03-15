fbfbccb2baba6e79e5e34567b7ceb25e0d2e50ad
FireLord <firelord.xda@gmail.com>
2024-03-15 15:54:03 +0530

    app: Create files and folders

---
+++ IosFirebaseLogin/presentation/screens/ForgotPasswordView.swift
@@ -0,0 +1,21 @@
+//
+//  ForgoutPasswordView.swift
+//  Soundscape
+//
+//  Created by admin on 04/03/24.
+//
+
+import SwiftUI
+
+struct ForgoutPasswordView: View {
+    @State var emailId: Binding<String> = .constant("")
+    
+    var body: some View {
+        VStack {
+        }
+    }
+}
+
+#Preview {
+    ForgoutPasswordView()
+}
---
+++ IosFirebaseLogin/presentation/screens/SignupView.swift
@@ -0,0 +1,18 @@
+//
+//  SignupView.swift
+//  IosFirebaseLogin
+//
+//  Created by admin on 15/03/24.
+//
+
+import SwiftUI
+
+struct SignupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignupView()
+}
